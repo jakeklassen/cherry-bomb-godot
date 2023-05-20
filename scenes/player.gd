683bf6f3ed90f79adb61529f5348850697c2b69b
Jake <jklassendev@gmail.com>
2023-05-20 09:53:31 -0400

    initial commit

---
+++ scenes/player.gd
@@ -0,0 +1,46 @@
+extends Area2D
+
+## Velocity in pixels per second
+@export var speed: int = 60;
+
+# Size of the game window.
+var screen_size;
+
+const BANK_LEFT_REGION = Rect2(8, 0, 8, 8);
+const BANK_RIGHT_REGION = Rect2(24, 0, 8, 8);
+const IDLE_REGION = Rect2(16, 0, 8, 8);
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	print_debug(screen_size);
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO;
+	
+	if Input.is_action_pressed("quit"):
+		get_tree().quit();
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x = 1;
+	
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -1;
+
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -1;
+	
+	if Input.is_action_pressed("move_down"):
+		velocity.y = 1;
+		
+	$Sprite2D.set_region_rect(IDLE_REGION);
+
+	if velocity.x > 0:
+		$Sprite2D.set_region_rect(BANK_RIGHT_REGION);
+	elif velocity.x < 0:
+		$Sprite2D.set_region_rect(BANK_LEFT_REGION);
+
+	position += velocity * speed * delta
+	position.x = clamp(position.x, 4, screen_size.x - 4)
+	position.y = clamp(position.y, 4, screen_size.y - 4)
---
+++ scenes/starfield.gd
@@ -0,0 +1,39 @@
+@tool
+
+extends Node2D
+
+@export var count: int = 100;
+
+var stars = [];
+var velocities = [20, 30, 60];
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for i in count:
+		var star = {
+			"color": Color.from_string("#fff1e8", Color.WHITE),
+			"position": Vector2(randi_range(1, 127), randi_range(1, 127)),
+			"speed": velocities.pick_random(),
+		}
+		
+		if star.speed < 30:
+			star.color = Color.from_string("#1d2b53", Color.WHITE);
+		elif star.speed < 60:
+			star.color = Color.from_string("#83769c", Color.WHITE);
+			
+		stars.push_back(star);
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	for star in stars:
+		star.position.y += star.speed * delta;
+		
+		if star.position.y > 128:
+			star.position.y = -1;
+			star.position.x = randi_range(1, 127);
+	
+	queue_redraw();
+
+func  _draw() -> void:
+	for star in stars:
+		draw_rect(Rect2(star.position.x, star.position.y, 1, 1), star.color, true);
