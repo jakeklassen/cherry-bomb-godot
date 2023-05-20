9739bbaabda97e8b13d059c788ac078b66c5c4fe
Jake <jklassendev@gmail.com>
2023-05-20 16:36:02 -0400

    messing with some screen switching

--- scripts/screen.gd
+++ scripts/screen.gd
@@ -1,10 +1,16 @@
 extends Node
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("quit"):
+		# At some point show a menu and don't just kill the game
 		get_tree().quit();
+
+	# https://docs.godotengine.org/en/stable/tutorials/scripting/change_scenes_manually.html 
+	if Input.is_anything_pressed():
+		var current_screen = self.name
+
+		if current_screen == "TitleScreen":
+			# Delay a little to avoid the abrupt sound cut off
+			await get_tree().create_timer(0.15).timeout
+			get_tree().change_scene_to_file("res://scenes/screens/game_screen.tscn")
