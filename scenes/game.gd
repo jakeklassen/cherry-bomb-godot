43f1233567a0b29bbd96e0dfada195166a6330b4
Jake <jklassendev@gmail.com>
2023-06-16 15:28:13 -0400

    tweak an enum

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-enum EGameState {
+enum GamePhase {
 	GameOver,
 	GameWon,
 	Playing,
@@ -13,15 +13,15 @@ enum EGameState {
 const GameplayScreen = preload("res://scenes/screens/game_screen.tscn")
 const TitleScreen = preload("res://scenes/screens/title_screen.tscn")
 
-var state := EGameState.Uninitialized
+var state := GamePhase.Uninitialized
 
 
 func _ready() -> void:
 	var master_sound = AudioServer.get_bus_index("Master")
 	AudioServer.set_bus_mute(master_sound, false)
 
-	if state == EGameState.Uninitialized:
-		state = EGameState.TitleScreen
+	if state == GamePhase.Uninitialized:
+		state = GamePhase.TitleScreen
 		get_tree().root.call_deferred("add_child", TitleScreen.instantiate())
 
 
@@ -30,14 +30,14 @@ func _process(_delta: float) -> void:
 		# At some point show a menu and don't just kill the game
 		get_tree().quit();
 
-	if Input.is_anything_pressed() and state == EGameState.TitleScreen and $AnyKeyPressedTimer.is_stopped():
+	if Input.is_anything_pressed() and state == GamePhase.TitleScreen and $AnyKeyPressedTimer.is_stopped():
 		$AnyKeyPressedTimer.start()
 		var current_screen = get_node("/root/TitleScreen")
 		if current_screen == null:
 			return
 
 		current_screen.queue_free()
-		state = EGameState.Playing
+		state = GamePhase.Playing
 		GameState.reset()
 
 		await get_tree().create_timer(0.2).timeout
@@ -46,11 +46,11 @@ func _process(_delta: float) -> void:
 		next_screen.connect("game_over", game_over)
 		get_tree().root.call_deferred("add_child", next_screen)
 
-	if Input.is_anything_pressed() and state == EGameState.GameOver and $AnyKeyPressedTimer.is_stopped():
+	if Input.is_anything_pressed() and state == GamePhase.GameOver and $AnyKeyPressedTimer.is_stopped():
 		$AnyKeyPressedTimer.start()
 		var current_screen = get_node("/root/GameScreen")
 		current_screen.queue_free()
-		state = EGameState.TitleScreen
+		state = GamePhase.TitleScreen
 
 		await get_tree().create_timer(0.2).timeout
 
@@ -59,4 +59,4 @@ func _process(_delta: float) -> void:
 
 
 func game_over() -> void:
-	state = EGameState.GameOver
+	state = GamePhase.GameOver
